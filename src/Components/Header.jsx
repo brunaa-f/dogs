f1fc14fa4174918113381d2018342ebdb28a0c37
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-01 15:00:34 -0300

    feat: created header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,14 +1,19 @@
-import React from 'react';
-import styles from './Header.module.css'
-import { Link } from 'react-router-dom';
+import React from "react";
+import styles from "./Header.module.css";
+import { Link } from "react-router-dom";
+import dogIcon from "/src/Assets/dogs.svg";
 
 const Header = () => {
-  return <div className={styles.header}>
-    <nav className='container'>
-      <Link to="/">Home</Link>
-      <Link to="/login">Login / Criar</Link>
-    </nav>
-  </div>;
+  return (
+    <header className={styles.header}>
+      <nav className={`${styles.nav} container`}>
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <img src={dogIcon} alt="Dog Icon" />
+        </Link>
+        <Link className={styles.login} to="/login">Login / Criar</Link>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
