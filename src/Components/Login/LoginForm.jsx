eb88a34f80fd35cb27ca3b3e10a203664467ebb5
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-01 21:41:46 -0300

    feat: email and password validation

--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,38 +1,43 @@
-import { func } from "prop-types";
-import React from "react";
-import { Link } from "react-router-dom";
-import Input from '../Forms/Input'
-import Button from '../Forms/Button'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-  const [userName, setUserName] = React.useState("");
-  const [password, setPassword] = React.useState("");
+  const username = useForm();
+  const password = useForm();
 
-  function handleSubmit(event){
+  function handleSubmit(event) {
     event.preventDefault();
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
         method: 'POST',
         headers: {
-            'Content-Type' : 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify({userName, password})
-    }).then(response =>{ 
-        console.log(response)
-        return respoonse.json();
-    }).then(json => {
-        console.log(json)
-    })
+        body: JSON.stringify(),
+      })
+        .then((response) => {
+          console.log(response);
+          return response.json();
+        })
+        .then((json) => {
+          console.log(json);
+        });
+    }
   }
 
   return (
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label='Usuário' type="text" name="username"/>
-        <Input label='Senha' type="password" name="password"/>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
-      <Link to="/login/create">Cadastro</Link>
+      <Link to="/login/criar">Cadastro</Link>
     </section>
   );
 };
