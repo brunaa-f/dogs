230493edce9e9d8fa738e860964e499fa3f50b58
Bruna Fernandes <109226923+brunaa-f@users.noreply.github.com>
2025-02-02 16:14:56 -0300

    feat: validade token

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -5,7 +5,7 @@ import dogIcon from "/src/Assets/dogs.svg";
 import { UserContext } from "../UserContext";
 
 const Header = () => {
-  const { data } = React.useContext(UserContext);
+  const { data, userLogout } = React.useContext(UserContext);
 
   return (
     <header className={styles.header}>
@@ -14,14 +14,18 @@ const Header = () => {
           <img src={dogIcon} alt="Dog Icon" />
         </Link>
         {data ? (
+          
           <Link className={styles.login} to="/conta">
             {data.nome}
+            <button onClick={userLogout}>Logout</button>
           </Link>
+          
         ) : (
           <Link className={styles.login} to="/login">
             Login / Criar
           </Link>
         )}
+      
       </nav>
     </header>
   );
--- src/Components/Login/LoginForm.jsx
+++ src/Components/Login/LoginForm.jsx
@@ -1,22 +1,21 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import Input from "../Forms/Input";
-import Button from "../Forms/Button";
-import useForm from "../../Hooks/useForm";
-import { TOKEN_POST, USER_GET } from "../../api";
-import { UserContext } from "../../UserContext";
+import React from 'react';
+import { Link } from 'react-router-dom';
+import Input from '../Forms/Input';
+import Button from '../Forms/Button';
+import useForm from '../../Hooks/useForm';
+import { UserContext } from '../../UserContext';
 
 const LoginForm = () => {
   const username = useForm();
   const password = useForm();
 
-  const {userLogin, data} = React.useContext(UserContext)
+  const { userLogin, error, loading } = React.useContext(UserContext);
 
   async function handleSubmit(event) {
     event.preventDefault();
 
     if (username.validate() && password.validate()) {
-      userLogin(username.value, password.value)
+      userLogin(username.value, password.value);
     }
   }
 
@@ -24,9 +23,14 @@ const LoginForm = () => {
     <section>
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
-        <Input label="User" type="text" name="username" {...username} />
-        <Input label="Password" type="password" name="password" {...password} />
-        <Button>Entrar</Button>
+        <Input label="Usuário" type="text" name="username" {...username} />
+        <Input label="Senha" type="password" name="password" {...password} />
+        {loading ? (
+          <Button disabled>Carregando...</Button>
+        ) : (
+          <Button>Entrar</Button>
+        )}
+        {error && <p>{error}</p>}
       </form>
       <Link to="/login/criar">Cadastro</Link>
     </section>
